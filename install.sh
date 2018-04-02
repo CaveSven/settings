903a00824a880851651adf6ae3eb45d9715ff39f
Sven Rohr <sven.rohr@tngtech.com>
2018-04-02 20:31:16 +0200

    Add ppa for ukuu

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
-apt update && apt upgrade && apt install git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu
+# ukuu
+add-apt-repository -y ppa:teejee2008/ppa
+
+apt update && apt upgrade && apt install -y git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu
+
 
