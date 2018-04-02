b11d2ecba26a73507fcbcff43d5577ce8ae5935e
Sven Rohr <sven.rohr@tngtech.com>
2018-04-02 22:17:19 +0200

    Add cmake

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # ukuu
 add-apt-repository -y ppa:teejee2008/ppa
 
-apt update && apt upgrade && apt install -y git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger
+apt update && apt upgrade && apt install -y cmake git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger
 
 
