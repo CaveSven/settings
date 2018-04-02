a35ca2a45b36fe027e142234b87fbef75ff414e2
Sven Rohr <sven.rohr@tngtech.com>
2018-04-02 21:39:50 +0200

    Add ranger

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # ukuu
 add-apt-repository -y ppa:teejee2008/ppa
 
-apt update && apt upgrade && apt install -y git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu
+apt update && apt upgrade && apt install -y git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger
 
 
