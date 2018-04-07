aba18e0dd4282b4321ea533746d386efce380382
Sven Rohr <sven.rohr@tngtech.com>
2018-04-07 20:06:14 +0200

    Add docker

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 # ukuu
 add-apt-repository -y ppa:teejee2008/ppa
 
-apt update && apt upgrade && apt install -y cmake git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger
+apt update && apt upgrade && apt install -y cmake git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger apt-transport-https ca-certificates curl software-properties-common 
 
 # ripgrep
 snap install rg
 
 # miniconda
 curl -o /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && chmod +x /tmp/miniconda.sh && /tmp/miniconda.sh -y
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update && apt install -y docker-ce
