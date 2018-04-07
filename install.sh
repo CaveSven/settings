a8804878da715486fea13af3bb09f764e1b4f93c
Sven Rohr <sven.rohr@tngtech.com>
2018-04-07 17:11:26 +0200

    Add miniconda

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ add-apt-repository -y ppa:teejee2008/ppa
 apt update && apt upgrade && apt install -y cmake git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger
 
 # ripgrep
-sudo snap install rg
+snap install rg
 
-
+# miniconda
+curl -o /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && chmod +x /tmp/miniconda.sh && /tmp/miniconda.sh -y
