7a5b9b97055def1f7ecf20c9ffff03edacf18e64
Sven Rohr <sven.rohr@tngtech.com>
2018-09-17 17:48:46 +0200

    Add tldr

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ add-apt-repository -y ppa:teejee2008/ppa
 # icons
 add-apt-repository -y ppa:moka/daily
 
-apt update && apt upgrade && apt install -y vim cmake git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger apt-transport-https ca-certificates curl software-properties-common atool fonts-font-awesome rofi thunar pavucontrol lxappearance arc-theme moka-icon-theme faba-icon-theme faba-mono-icons arandr openconnect network-manager-openconnect network-manager-openconnect-gnome network-manager-openvpn-gnome duplicity python-gi dconf-cli ffmpeg xbacklight grub2 tree gsimplecal
+apt update && apt upgrade && apt install -y vim cmake git i3 keepassx feh i3blocks compton htop pydf mtr ncdu autojump ukuu ranger apt-transport-https ca-certificates curl software-properties-common atool fonts-font-awesome rofi thunar pavucontrol lxappearance arc-theme moka-icon-theme faba-icon-theme faba-mono-icons arandr openconnect network-manager-openconnect network-manager-openconnect-gnome network-manager-openvpn-gnome duplicity python-gi dconf-cli ffmpeg xbacklight grub2 tree gsimplecal firewalld
 
 # ripgrep
 snap install rg
@@ -18,3 +18,6 @@ curl -o /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update && apt install -y docker-ce
+
+# tldr
+npm install -g tldr
