c73bf9d369f1a2264f2e7703f69c016d0f276da0
Sven Rohr <sven.rohr@tngtech.com>
2019-08-21 08:47:46 +0200

    Insatll mdp, dvc, sd

---
+++ install_manjaro.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+sudo pacman -S vim cmake git i3 keepassxc feh i3blocks compton htop pydf mtr ncdu ranger atool ttf-font-awesome rofi thunar pavucontrol lxappearance arc-theme moka-icon-theme faba-icon-theme faba-mono-icons arandr duplicity tree gsimplecal nmap ufw fd ripgrep fasd bat tldr sd
+
+# markdown viewer for shell
+pip install mdv mdp dvc
